da131126edf62fe09ba3f2daa84316a3b8273036
Charley Chu <haoc@cypress.com>
2020-12-21 11:44:10 -0800

    Revert "Remove iot prefix."
    This reverts commit 1ba02bb26f2b5e11fdd6d8b455bea938eee05679.

--- iot_pkcs11_psa_object_management.h
+++ iot_pkcs11_psa_object_management.h
@@ -33,8 +33,8 @@
 #include <stdbool.h>
 
 /* PKCS#11 includes. */
-#include "core_pkcs11_config.h"
-#include "core_pkcs11.h"
+#include "iot_pkcs11_config.h"
+#include "iot_pkcs11.h"
 
 /* PSA includes. */
 #include "psa/crypto.h"
